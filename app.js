478f63aafaa83b40b99a4df74ab2923d79c2794d
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2024-03-23 01:39:25 +0800

    smooth scroll added

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// SmoothScroll
+const scroll = new SmoothScroll('.main-nav a[href*="#"]', { speed: 500 });
+const allNavLinks = document.querySelectorAll('.main-nav a[href*="#"]');
+allNavLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
